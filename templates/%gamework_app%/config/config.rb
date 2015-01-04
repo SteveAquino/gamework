c5d64a8518810a418ea9ac557db5b970fe32245b
Steve Aquino <aquino.steve@gmail.com>
2015-01-04 00:05:09 -0800

    More default config options'

--- templates/%gamework_app%/config/config.rb
+++ templates/%gamework_app%/config/config.rb
@@ -3,5 +3,10 @@ Gamework::App.config do |c|
   c.height = 640
   c.title  = "Ruby Zelda"
   c.asset_directory = File.expand_path 'media'
+
+  # Log console output
+  # c.log_file = File.expand_path 'log/log.txt'
+
+  # Enable debug mode
   # c.debug_mode = true
 end
